39de4df3434b1fa1a39522c31def8bab1ef129b3
Dash <89717536+anthempart2@users.noreply.github.com>
2024-01-23 21:13:27 +0530

    updated!

--- creating files and folders/creatingFolders.java
+++ creating files and folders/creatingFolders.java
@@ -1,1 +1,20 @@
 // this is a random comment .
+import java.io.File;
+
+public class creatingFolders {
+    public static void main(String args[]){
+        //specifying the path of my new folder
+        String folderPath = "D:/";
+
+        File folder = new File(folderPath);
+
+        boolean success = folder.mkdir();
+
+        if(success) {
+            System.out.println("Folder created!");
+        }
+        else{
+            System.out.println("Error!");
+        }
+    }
+}
