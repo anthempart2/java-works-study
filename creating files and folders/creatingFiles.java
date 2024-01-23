9083352a4d049b1ca3863d043415bf144939f71e
Dash <89717536+anthempart2@users.noreply.github.com>
2024-01-24 01:15:12 +0530

    updated!

--- creating files and folders/creatingFiles.java
+++ creating files and folders/creatingFiles.java
@@ -1,9 +1,20 @@
+//code to create files using only java commands
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class creatingFiles {
     public static void main(String args[]) {
-        String filePath = "D:\\FileNameSample"
+        //Add the filename alongside the path
+        //here for instance it is FileNameSample
+        String filePath = "D:\\FolderNameSample\\FileNameSample.txt";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))){
+            //content you want in the file
+            writer.write("Hello, greetings from my file!");
+        } catch (IOException e){
+            System.out.println("Error creating file!" + e.getMessage());
+        }
     }
 }
