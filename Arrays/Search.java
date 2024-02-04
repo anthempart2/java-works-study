77361f90e29a04032e4e571137f931912d97e534
Dash <89717536+anthempart2@users.noreply.github.com>
2024-02-04 23:16:28 +0530

    upd!

---
+++ Arrays/Search.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class Search {
+    public static void main(String[] args) {
+        int n = 10;
+        int[] arr = new int[n];
+
+        System.out.println("Program works!");
+    }
+}
